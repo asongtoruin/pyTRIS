fe01fd1e201f1f7735322fd3ba497b20f6bf10b0
Adam <adam.ruszkowski@gmail.com>
2020-07-25 14:19:20 +0100

    Add to and refactor area tests
    Isolate the check that the endpoint exists, and add in a new check for
    getting a single area.

--- tests/test_areas.py
+++ tests/test_areas.py
@@ -1,13 +1,25 @@
 import pytest
 
-from pytris.api import API, KNOWN_VERSIONS
 from pytris.models import Area
 
 
+def test_areas_available(latest_api):
+    assert callable(getattr(latest_api, 'areas', None))
+
 @pytest.mark.vcr()
 def test_get_all_areas(latest_api):
+    assert callable(getattr(latest_api.areas(), 'all', None))
 
     res = latest_api.areas().all()
 
     # assert isinstance(res, tuple)
     assert all(isinstance(a, Area) for a in res)
+
+@pytest.mark.vcr()
+def test_get_one_area(latest_api):
+    assert callable(getattr(latest_api.areas(), 'get', None))
+
+    res = latest_api.areas().get(1)
+
+    # assert isinstance(res, tuple)
+    assert isinstance(res, Area)
